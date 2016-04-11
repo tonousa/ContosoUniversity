ca0f600e5216835b4742f9a452be7ca3c3ce63e7
tonousa <tonousa@hotmail.com>
2016-04-11 17:08:51 -0400

    pa 137

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [StringLength(50, MinimumLength = 1)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage="Name no longer than 50 chars")]
+        [Display(Name = "First Name")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
+        // DataType specifies a type more specific than the db type
+        // It doesn't provide validation
+        [DataType(DataType.Date)]
+        // Explicitly specifies date format
+        [DisplayFormat(DataFormatString = "{0:yyy-MM-dd}",
+            ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+        [Display(Name="Full Name")]
+        public string FullName {
+            get 
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
 }
