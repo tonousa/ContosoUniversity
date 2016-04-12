21d2ef8c9421d2ca78903b7ed184c68eae0720b0
tonousa <tonousa@hotmail.com>
2016-04-12 09:34:59 -0400

    pg 143

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace ContosoUniversity.Models
 {
     public enum Grade
@@ -10,6 +11,7 @@ namespace ContosoUniversity.Models
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText="No grade")]
         public Grade? Grade { get; set; }
 
         public virtual Course Course { get; set; }
