bbec04d9e66e2e89b5705fff671de50a6ef06c67
tonousa <tonousa@hotmail.com>
2016-04-18 19:58:00 -0400

    pg 202

--- ContosoUniversity/Controllers/InstructorController.cs
+++ ContosoUniversity/Controllers/InstructorController.cs
@@ -140,9 +140,9 @@ namespace ContosoUniversity.Controllers
         // POST: /Instructor/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost, ActionName("Edit")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult EditPost(int? id, string[] selectedCourses)
+        public ActionResult Edit(int? id, string[] selectedCourses)
         {
             if (id == null)
             {
