ef4a01461faff95b3f49b26e224dcbc4b80a53f2
tonousa <tonousa@hotmail.com>
2016-04-20 09:26:30 -0400

    page 219

--- ContosoUniversity/Models/Department.cs
+++ ContosoUniversity/Models/Department.cs
@@ -18,7 +18,7 @@ namespace ContosoUniversity.Models
         public decimal Budget { get; set; }
 
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "0:yyyy-MM-dd", 
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", 
             ApplyFormatInEditMode = true)]
         [Display(Name="Start Date")]
         public DateTime StartDate { get; set; }
